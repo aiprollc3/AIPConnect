df5a96d7e7183accf897f9d24a601586ec1a2f4a
Ambassadors In Productions <Aiprollc3@gmail.com>
2025-11-12 07:11:31 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        accent: '#FFD700'
+      },
+    },
+  },
+  plugins: [],
+}
